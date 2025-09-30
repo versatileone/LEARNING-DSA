7e3aae8f392d16e7555757b56dcb9872220f8f99
versatileone <sharmadi.kota@gmail.com>
2025-09-30 11:19:20 +0530

    Add files via upload

---
+++ sort012-in-array.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void printarray(int arr[],int size){
+    int i=0;
+    while(i<size){
+        cout<<arr[i]<<",";
+        i++;
+    }
+    cout<<endl;
+}
+
+
+
+int main(){
+    int arr[10]={1,2,1,2,1,2,1,0,0,0};
+    int size=10;
+    int i =0,j=size-1;
+    while(i<j){
+        if(arr[i]==2){
+
+            if(arr[j]!=2){
+                swap(arr[i],arr[j]);
+                    while(arr[j]==2){
+                        j--;
+
+                    }
+            }
+            
+        }
+        i++;
+
+    }
+    i=0,j=size-1;
+    while(i<j){
+        if(arr[j]==2 || arr[j]==1){
+            j--;
+        }
+        if(arr[i]==0){
+            i++;
+        }
+        if(arr[i]==1 && arr[j]==0){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+
+    }
+     printarray(arr,size);
+   
+
+
+
+}
